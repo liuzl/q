cc5d7de4dfa19f6ca812caabc63cd43308034d50
liuzhanliang <zhanliangliu@qq.com>
2018-07-01 08:40:52 +0800

    t -> q

--- queue.go
+++ queue.go
@@ -12,8 +12,6 @@ import (
 	"github.com/syndtr/goleveldb/leveldb/util"
 )
 
-var debug = false
-
 type Queue struct {
 	Name string `json:"name"`
 	Dir  string `json:dir`
@@ -25,24 +23,24 @@ type Queue struct {
 }
 
 func NewQueue(name, dir string) (*Queue, error) {
-	t := &Queue{Name: name, Dir: dir, exit: make(chan bool)}
+	q := &Queue{Name: name, Dir: dir, exit: make(chan bool)}
 	var err error
 	queueDir := filepath.Join(dir, name, "queue")
-	if t.queue, err = ds.OpenQueue(queueDir); err != nil {
+	if q.queue, err = ds.OpenQueue(queueDir); err != nil {
 		return nil, err
 	}
 	retryDir := filepath.Join(dir, name, "retry_queue")
-	if t.retryQueue, err = ds.OpenQueue(retryDir); err != nil {
+	if q.retryQueue, err = ds.OpenQueue(retryDir); err != nil {
 		return nil, err
 	}
 	storeDir := filepath.Join(dir, name, "running")
-	if t.runningStore, err = store.NewLevelStore(storeDir); err != nil {
+	if q.runningStore, err = store.NewLevelStore(storeDir); err != nil {
 		return nil, err
 	}
 
-	go t.retry()
+	go q.retry()
 
-	return t, nil
+	return q, nil
 }
 
 func (q *Queue) Status() map[string]interface{} {
@@ -55,19 +53,16 @@ func (q *Queue) Status() map[string]interface{} {
 	}
 }
 
-func (t *Queue) Enqueue(data string) error {
-	if debug {
-		fmt.Println("Enqueue:", data, t.queue.Length(), t.retryQueue.Length())
-	}
-	if t.queue != nil {
-		_, err := t.queue.EnqueueString(data)
+func (q *Queue) Enqueue(data string) error {
+	if q.queue != nil {
+		_, err := q.queue.EnqueueString(data)
 		return err
 	}
 	return fmt.Errorf("queue is nil")
 }
 
-func (t *Queue) dequeue(q *ds.Queue, timeout int64) (string, string, error) {
-	item, err := q.Dequeue()
+func (q *Queue) dequeue(queue *ds.Queue, timeout int64) (string, string, error) {
+	item, err := queue.Dequeue()
 	if err != nil {
 		return "", "", err
 	}
@@ -75,83 +70,77 @@ func (t *Queue) dequeue(q *ds.Queue, timeout int64) (string, string, error) {
 	if timeout > 0 {
 		now := time.Now().Unix()
 		key = goutil.TimeStr(now+timeout) + ":" + goutil.ContentMD5(item.Value)
-		if err = t.addToRunning(key, item.Value); err != nil {
+		if err = q.addToRunning(key, item.Value); err != nil {
 			return "", "", err
 		}
 	}
 	return key, string(item.Value), nil
 }
 
-func (t *Queue) Dequeue(timeout int64) (string, string, error) {
-	if t.retryQueue != nil && t.retryQueue.Length() > 0 {
-		return t.dequeue(t.retryQueue, timeout)
+func (q *Queue) Dequeue(timeout int64) (string, string, error) {
+	if q.retryQueue != nil && q.retryQueue.Length() > 0 {
+		return q.dequeue(q.retryQueue, timeout)
 	}
-	if t.queue != nil && t.queue.Length() > 0 {
-		return t.dequeue(t.queue, timeout)
+	if q.queue != nil && q.queue.Length() > 0 {
+		return q.dequeue(q.queue, timeout)
 	}
 	return "", "", fmt.Errorf("Queue is empty")
 }
 
-func (t *Queue) Confirm(key string) error {
-	if debug {
-		fmt.Println("confirm", key, t.queue.Length(), t.retryQueue.Length())
-	}
-	if t.runningStore == nil {
+func (q *Queue) Confirm(key string) error {
+	if q.runningStore == nil {
 		return fmt.Errorf("runningStore is nil")
 	}
-	return t.runningStore.Delete(key)
+	return q.runningStore.Delete(key)
 }
 
-func (t *Queue) Close() {
-	if t.exit != nil {
-		t.exit <- true
+func (q *Queue) Close() {
+	if q.exit != nil {
+		q.exit <- true
 	}
-	if t.queue != nil {
-		t.queue.Close()
+	if q.queue != nil {
+		q.queue.Close()
 	}
-	if t.retryQueue != nil {
-		t.retryQueue.Close()
+	if q.retryQueue != nil {
+		q.retryQueue.Close()
 	}
-	if t.runningStore != nil {
-		t.runningStore.Close()
+	if q.runningStore != nil {
+		q.runningStore.Close()
 	}
 }
 
-func (t *Queue) Drop() {
-	t.Close()
-	path := filepath.Join(t.Dir, t.Name)
+func (q *Queue) Drop() {
+	q.Close()
+	path := filepath.Join(q.Dir, q.Name)
 	os.RemoveAll(path)
 }
 
-func (t *Queue) addToRunning(key string, value []byte) error {
-	if debug {
-		fmt.Println("addToRunning", key, t.queue.Length(), t.retryQueue.Length())
-	}
+func (q *Queue) addToRunning(key string, value []byte) error {
 	if len(value) == 0 {
 		return fmt.Errorf("empty value")
 	}
-	if t.runningStore == nil {
+	if q.runningStore == nil {
 		return fmt.Errorf("runningStore is nil")
 	}
-	return t.runningStore.Put(key, value)
+	return q.runningStore.Put(key, value)
 }
 
-func (t *Queue) retry() {
+func (q *Queue) retry() {
 	for {
 		select {
-		case <-t.exit:
+		case <-q.exit:
 			return
 		default:
 			now := time.Now().Format("20060102030405")
-			t.runningStore.ForEach(&util.Range{Limit: []byte(now)},
+			q.runningStore.ForEach(&util.Range{Limit: []byte(now)},
 				func(key, value []byte) (bool, error) {
-					if _, err := t.retryQueue.Enqueue(value); err != nil {
+					if _, err := q.retryQueue.Enqueue(value); err != nil {
 						return false, err
 					}
-					t.runningStore.Delete(string(key))
+					q.runningStore.Delete(string(key))
 					return true, nil
 				})
-			goutil.Sleep(5*time.Second, t.exit)
+			goutil.Sleep(5*time.Second, q.exit)
 		}
 	}
 }
