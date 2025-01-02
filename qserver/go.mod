c86258aa67e5cee27e4a39d928c62959862b7643
liuzl <liang@zliu.org>
2025-01-02 12:52:01 +0800

    Update qserver to use Fuego v0.17.0 and fix paths.

--- qserver/go.mod
+++ qserver/go.mod
@@ -1,24 +1,23 @@
 module qserver
 
-go 1.22.2
+go 1.23.3
 
 require (
-	github.com/go-fuego/fuego v0.14.0
-	github.com/golang/glog v1.2.3
-	github.com/liuzl/q v0.0.0-20241102160036-64f1890546d0
-	zliu.org/goutil v0.0.0-20241031150925-efd2494eb218
+	github.com/go-fuego/fuego v0.17.0
+	github.com/liuzl/q v0.0.0-20250102040822-21ad339470db
+	zliu.org/goutil v0.0.0-20241204031207-4d1c3f4d77e7
 )
 
 require (
-	github.com/carlmjohnson/requests v0.24.2 // indirect
+	github.com/carlmjohnson/requests v0.24.3 // indirect
 	github.com/eknkc/basex v1.0.1 // indirect
-	github.com/gabriel-vasile/mimetype v1.4.6 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.7 // indirect
 	github.com/getkin/kin-openapi v0.128.0 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.22.1 // indirect
+	github.com/go-playground/validator/v10 v10.23.0 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/gorilla/schema v1.4.1 // indirect
@@ -29,7 +28,7 @@ require (
 	github.com/liuzl/ds v0.0.0-20241110115610-698361118af0 // indirect
 	github.com/liuzl/filestore v0.0.0-20200229104338-5ea723a6a528 // indirect
 	github.com/liuzl/store v0.0.0-20190530065605-e2dbcd3c77fc // indirect
-	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mailru/easyjson v0.9.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
@@ -37,11 +36,9 @@ require (
 	github.com/rs/xid v1.6.0 // indirect
 	github.com/rs/zerolog v1.33.0 // indirect
 	github.com/syndtr/goleveldb v1.0.0 // indirect
-	golang.org/x/crypto v0.28.0 // indirect
-	golang.org/x/net v0.30.0 // indirect
-	golang.org/x/sys v0.26.0 // indirect
-	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
-
-replace github.com/liuzl/q => ..
--- qserver/main.go
+++ qserver/main.go
@@ -2,131 +2,55 @@ package main
 
 import (
 	"flag"
-	"net/http"
-	"strconv"
-	"strings"
 
 	"github.com/go-fuego/fuego"
-	"github.com/golang/glog"
+	"github.com/go-fuego/fuego/option"
+	"github.com/go-fuego/fuego/param"
 	"github.com/liuzl/q"
 	"zliu.org/goutil/rest"
 )
 
 var (
-	addr  = flag.String("addr", "127.0.0.1:9080", "bind address")
-	path  = flag.String("path", "./queue", "task queue dir")
-	queue *q.Queue
+	addr   = flag.String("addr", "127.0.0.1:9080", "bind address")
+	path   = flag.String("path", "./queue", "task queue dir")
+	server *q.QServer
+	zlog   = rest.Log()
 )
-
-// 创建任务
-func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
-	glog.Infof("addr=%s  method=%s host=%s uri=%s",
-		r.RemoteAddr, r.Method, r.Host, r.RequestURI)
-	r.ParseForm()
-	data := strings.TrimSpace(r.FormValue("data"))
-	if data == "" {
-		rest.ErrBadRequest(w, "data is empty")
-		return
-	}
-	if err := queue.Enqueue(data); err != nil {
-		rest.ErrInternalServer(w, err)
-		return
-	}
-	rest.MustEncode(w, map[string]string{"status": "ok"})
-}
-
-// 获取任务
-func GetTaskHandler(w http.ResponseWriter, r *http.Request) {
-	glog.Infof("addr=%s  method=%s host=%s uri=%s",
-		r.RemoteAddr, r.Method, r.Host, r.RequestURI)
-	r.ParseForm()
-	t := strings.TrimSpace(r.FormValue("timeout"))
-	timeout, err := strconv.ParseInt(t, 10, 64)
-	if err != nil {
-		rest.ErrBadRequest(w, "invalid timeout")
-		return
-	}
-	key, value, err := queue.Dequeue(timeout)
-	if err != nil {
-		if err.Error() == "Queue is empty" {
-			rest.ErrorMessageWithStatus(w, err.Error(), http.StatusNotFound)
-		} else {
-			rest.ErrInternalServer(w, err.Error())
-		}
-		return
-	}
-	rest.MustEncode(w, map[string]string{"key": key, "value": value})
-}
-
-// 更新任务状态
-func UpdateTaskStatusHandler(w http.ResponseWriter, r *http.Request) {
-	glog.Infof("addr=%s  method=%s host=%s uri=%s",
-		r.RemoteAddr, r.Method, r.Host, r.RequestURI)
-
-	key := r.PathValue("key")
-	if key == "" {
-		rest.ErrBadRequest(w, "empty key")
-		return
-	}
-	if err := queue.Confirm(key); err != nil {
-		if strings.HasPrefix(err.Error(), "key not found") {
-			rest.ErrorMessageWithStatus(w, err.Error(), http.StatusNotFound)
-		} else {
-			rest.ErrInternalServer(w, err)
-		}
-		return
-	}
-	rest.MustEncode(w, map[string]string{"status": "ok"})
-}
-
-// 获取队列状态
-func GetQueueStatusHandler(w http.ResponseWriter, r *http.Request) {
-	glog.Infof("addr=%s  method=%s host=%s uri=%s",
-		r.RemoteAddr, r.Method, r.Host, r.RequestURI)
-	rest.MustEncode(w, queue.Status())
-}
-
-// 获取下一个任务预览
-func GetNextTaskHandler(w http.ResponseWriter, r *http.Request) {
-	glog.Infof("addr=%s  method=%s host=%s uri=%s",
-		r.RemoteAddr, r.Method, r.Host, r.RequestURI)
-	r.ParseForm()
-	value, err := queue.Peek()
-	if err != nil {
-		if err.Error() == "Queue is empty" {
-			rest.ErrorMessageWithStatus(w, err.Error(), http.StatusNotFound)
-		} else {
-			rest.ErrInternalServer(w, err.Error())
-		}
-		return
-	}
-	rest.MustEncode(w, map[string]string{"value": value})
-}
 
 func main() {
 	flag.Parse()
-	defer glog.Flush()
+
 	var err error
-	if queue, err = q.NewQueue(*path); err != nil {
-		glog.Fatal(err)
+	if server, err = q.NewQServer(*path); err != nil {
+		zlog.Fatal().Err(err).Msg("failed to create qserver")
 	}
-	defer queue.Close()
-	defer glog.Info("server exit")
+	defer server.Close()
+	defer zlog.Info().Msg("server exit")
 
 	s := fuego.NewServer(fuego.WithAddr(*addr))
 
-	fuego.PostStd(s, "/tasks", CreateTaskHandler).
-		Param("query", "data", "Data to enqueue", fuego.OpenAPIParam{Required: true, Type: "string"})
+	fuego.PostStd(s, "/tasks", server.CreateTaskHandler,
+		option.Summary("Create a task"),
+		option.Description("Create a task to be processed by the queue"),
+		option.OperationID("createTask"),
+		option.Tags("tasks"),
+		option.Query("data", "Data to enqueue"),
+	)
 
-	fuego.GetStd(s, "/tasks", GetTaskHandler).
-		Param("query", "timeout", "Timeout in seconds", fuego.OpenAPIParam{Type: "int", Example: "300"})
+	fuego.GetStd(s, "/tasks", server.GetTaskHandler,
+		option.Summary("Get a task"),
+		option.Description("Get a task from the queue"),
+		option.OperationID("getTask"),
+		option.Tags("tasks"),
+		option.QueryInt("timeout", "Timeout in seconds", param.Default(300)),
+	)
 
-	fuego.PutStd(s, "/tasks/{key}", UpdateTaskStatusHandler)
+	fuego.PutStd(s, "/tasks/{key}", server.UpdateTaskStatusHandler)
 
-	fuego.GetStd(s, "/tasks/status", GetQueueStatusHandler)
+	fuego.GetStd(s, "/tasks/status", server.GetQueueStatusHandler)
 
-	fuego.GetStd(s, "/tasks/next", GetNextTaskHandler)
+	fuego.GetStd(s, "/tasks/next", server.GetNextTaskHandler)
 
-	glog.Info("qserver listen on", *addr)
+	zlog.Info().Str("addr", *addr).Msg("qserver listen on")
 	s.Run()
 }
